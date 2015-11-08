7135f4bccdb81b11416f982e8e872ccbdfdc8dfb
sunilp <sunil.pulugula@wavemaker.com>
2015-11-08 13:59:01 +0530

    Example for creating proxy using method interceptor.

---
+++ src/main/java/com/cglib/model/JobsManager.java
@@ -0,0 +1,34 @@
+package com.cglib.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author <a href="mailto:sunil.pulugula@wavemaker.com">Sunil Kumar</a>
+ * @since 8/11/15
+ */
+public class JobsManager {
+
+    private static List<String> jobs = new ArrayList<String>();
+
+    static {
+        jobs.add("job1");
+        jobs.add("job2");
+        jobs.add("job3");
+    }
+
+    public List<String> getAllJobs() {
+        return jobs;
+    }
+
+    public int getCount() {
+        return jobs.size();
+    }
+
+    public List<String> createJob(String name) {
+        jobs.add(name);
+        return jobs;
+    }
+
+
+}
